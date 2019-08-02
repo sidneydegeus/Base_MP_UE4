913e2ed995de1e87038c57cb17919176f0998739
Sidney de Geus <sidneydegeus@hotmail.com>
2019-08-02 16:17:01 +0200

    deleleted old menu system, reimported adjusted menu system

--- Source/Base_MP_UE4/BaseMPGameInstance.h
+++ Source/Base_MP_UE4/BaseMPGameInstance.h
@@ -3,39 +3,15 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Engine/GameInstance.h"
-#include "MenuSystem/MainMenuInterface.h"
+#include "MenuSystem/MenuSystemGameInstance.h"
 #include "BaseMPGameInstance.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class BASE_MP_UE4_API UBaseMPGameInstance : public UGameInstance, public IMainMenuInterface
+class BASE_MP_UE4_API UBaseMPGameInstance : public UMenuSystemGameInstance
 {
 	GENERATED_BODY()
-
-// Variables
-private:
-	TSubclassOf<class UUserWidget> MainMenuClass;
-	TSubclassOf<class UUserWidget> InGameMenuClass;
-
-// Functions
-public:
-	UBaseMPGameInstance(const FObjectInitializer & ObjectInitializer);
-
-	virtual void Init();
-
-	UFUNCTION(BlueprintCallable)
-		void LoadMainMenu();
-
-	UFUNCTION(BlueprintCallable)
-		void LoadInGameMenu();
-
-	UFUNCTION(Exec)
-		void Host() override;
-
-	UFUNCTION(Exec)
-		void Join(const FString& Address) override;
-
+	
 };
